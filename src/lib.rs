20a705d087128e438a236c8bcb71aefceac82744
LNSEAB <691752+LNSEAB@users.noreply.github.com>
2022-11-23 08:39:32 +0900

    fmt

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@
 
 mod context;
 mod device;
+mod error;
 pub mod events;
 mod geometry;
 pub mod ime;
@@ -10,17 +11,16 @@ pub mod raw_input;
 mod ui_thread;
 mod utility;
 mod window;
-mod error;
 
 use context::Context;
 
 pub use device::*;
+pub use error::{Error, Result};
 #[doc(inline)]
 pub use events::{Event, ResizingEdge};
 pub use geometry::*;
 pub use ui_thread::UiThread;
 pub use window::{EventReceiver, Window, WindowBuilder, WindowStyle};
-pub use error::{Error, Result};
 
 #[inline]
 pub fn init() {
