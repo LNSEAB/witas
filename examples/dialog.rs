ec9c1ae185849c68708f43d191dbff23b4ffb9fd
LNSEAB <691752+LNSEAB@users.noreply.github.com>
2022-12-05 17:56:21 +0900

    修正

--- examples/dialog.rs
+++ examples/dialog.rs
@@ -9,25 +9,27 @@ async fn main() {
     loop {
         let event = rx.recv().await;
         match event {
-            witas::Event::KeyInput(input) => match input.key_code.vkey {
-                witas::VirtualKey::O => {
-                    let path = witas::FileOpenDialog::new().show().await.unwrap();
-                    println!("Open: {:?}", path);
+            witas::Event::KeyInput(input) if input.key_state == witas::KeyState::Pressed => {
+                match input.key_code.vkey {
+                    witas::VirtualKey::O => {
+                        let path = witas::FileOpenDialog::new().show().await.unwrap();
+                        println!("Open: {:?}", path);
+                    }
+                    witas::VirtualKey::M => {
+                        let paths = witas::FileOpenDialog::new()
+                            .allow_multi_select()
+                            .show()
+                            .await
+                            .unwrap();
+                        println!("Open(Multi): {:?}", paths);
+                    }
+                    witas::VirtualKey::S => {
+                        let path = witas::FileSaveDialog::new().show().await.unwrap();
+                        println!("Save: {:?}", path);
+                    }
+                    _ => {}
                 }
-                witas::VirtualKey::M => {
-                    let paths = witas::FileOpenDialog::new()
-                        .allow_multi_select()
-                        .show()
-                        .await
-                        .unwrap();
-                    println!("Open(Multi): {:?}", paths);
-                }
-                witas::VirtualKey::S => {
-                    let path = witas::FileSaveDialog::new().show().await.unwrap();
-                    println!("Save: {:?}", path);
-                }
-                _ => {}
-            },
+            }
             witas::Event::Quit => break,
             _ => {}
         }
